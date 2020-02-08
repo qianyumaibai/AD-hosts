0c370d86854cce29ba720847e308b5d330255e3c
E7KMbb <34418195+E7KMbb@users.noreply.github.com>
2020-02-08 17:42:25 +0800

    Add files via upload

---
+++ service.sh
@@ -0,0 +1,6 @@
+# This script will be executed in late_start service mode
+# More info in the main Magisk thread
+#set permissions
+chmod 600 $MODPATH/./system/etc/hosts
+set_perm $MODPATH/system/etc/hosts 0 0 0600
+set_perm $tmpModPath/system/etc/hosts 0 0 0600
