8647917c064257ac5d3a03c19beee88d3e7f7690
E7KMbb <34418195+E7KMbb@users.noreply.github.com>
2020-03-06 10:36:45 +0800

    Add files via upload
    非国内版MIUI不用选择

--- common/install.sh
+++ common/install.sh
@@ -1,3 +1,5 @@
+var_miui="`grep_prop ro.miui.build.region`"
+if [ "`echo $var_miui`" = "cn" ]; then
   ui_print " "
   ui_print "是否加入api.ad.xiaomi.com"
   ui_print "加入教导致小米应用商城里的积分商城与红包功能无法使用"
@@ -6,8 +8,11 @@
   ui_print "  音量- = 不加入"
   if $VKSEL; then
     ui_print "已选择加入"
-    ui_print "正在写入中..."
+    ui_print "正在写入中....."
     sed -i "s/<adxiaomi>/api.ad.xiaomi.com/g" $MODPATH/system/etc/hosts
   else
     ui_print "已选择不加入"
   fi
+else
+  ui_print " "
+fi
