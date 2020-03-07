8851bb206a6a99a2dd4fb2b35a0740874d27bbfa
E7KMbb <34418195+E7KMbb@users.noreply.github.com>
2020-03-07 08:34:31 +0800

    Add files via upload

--- common/addon/Volume-Key-Selector/preinstall.sh
+++ common/addon/Volume-Key-Selector/preinstall.sh
@@ -6,7 +6,7 @@ export PATH=$MODPATH/common/addon/Volume-Key-Selector/tools/$ARCH32:$PATH
 
 keytest() {
   ui_print "- 音量键测试"
-  ui_print "   请按音量键:"
+  ui_print "   请按音量上键:"
   if (timeout 3 /system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $TMPDIR/events); then
     return 0
   else
