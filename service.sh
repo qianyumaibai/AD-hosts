d9951243c886bc9f717a451e4ac81097875f78cc
E7KMbb <2984227069@qq.com>
2021-05-30 01:33:09 +0000

    fix bug

--- service.sh
+++ service.sh
@@ -16,10 +16,10 @@ until [ $(getprop sys.boot_completed) -eq 1 ] && [ -d "$work_dir" ]; do
   fi
 done
 
-. $MODDIR/script/select.ini
+sh $MODDIR/script/select.ini
 if [ $update_boot_start = "true" ]; then
-. $MODDIR/script/functions.sh
+sh $MODDIR/script/functions.sh
 fi
 if [ $regular_update_boot_start = "true" ]; then
-. $MODDIR/script/cron.sh
+sh $MODDIR/script/cron.sh
 fi
