3c9812daa54a375ab2d26bbdde819b83e2631317
E7KMbb <2984227069@qq.com>
2021-07-25 14:33:27 +0000

    改进定时更新

--- script/cron.sh
+++ script/cron.sh
@@ -12,7 +12,45 @@ if [ $regular_update = "on" ]; then
    done
    rm -rf $script_dir/crontabs/root
    touch $script_dir/crontabs/root
-   echo "$M $H $DOM $MO $DOW  sh $script_dir/functions.sh" >> $script_dir/crontabs/root
+   if [[ $time_format = "24" || $time_format = "AM" ]]; then
+      H="$(echo $time | awk -F ':' '{print $1}')"
+      M="$(echo $time | awk -F ':' '{print $2}')"
+      if [ $M = "00" ]; then
+         export M="0"
+      elif $(echo "$M" | grep -q '^0'); then
+         export M="$(echo $time | awk -F ':' '{print $2}' | awk -F '0' '{print $2}')"
+      fi
+   elif [ $time_format = "PM" ]; then
+      H="$(($(echo $time | awk -F ':' '{print $1}') + 12))"
+      M="$(echo $time | awk -F ':' '{print $2}')
+      if $(echo "$H" | grep -q '^0'); then
+         export H="$(echo $time | awk -F ':' '{print $1}' | awk -F '0' '{print $2}')"
+      fi
+      if [ $M = "00" ]; then
+         export M="0"
+      elif $(echo "$M" | grep -q '^0'); then
+         export M="$(echo $time | awk -F ':' '{print $2}' | awk -F '0' '{print $2}')"
+      fi
+   fi
+   if [ $wupdate = "y" ]; then
+      DOW="$wday"
+   elif [ $wupdate = "n" ]; then
+      DOW="*"
+   else
+      echo "错误参数:$wupdate"
+      echo "请输入 y/n"
+      exit 1
+   fi
+   if [ $mupdate = "y" ]; then
+      DOM="$wdate"
+   elif [ $mupdate = "n" ]; then
+      DOM="*"
+   else
+      echo "错误参数:$mupdate"
+      echo "请输入 y/n"
+      exit 1
+   fi
+   echo "$M $H $DOM * $DOW  sh $script_dir/functions.sh" >> $script_dir/crontabs/root
    chmod 777 $script_dir/crontabs/root
    crond -b -c $script_dir/crontabs
    echo "已开启定时更新服务"
@@ -24,5 +62,6 @@ elif [ $regular_update = "off" ]; then
    echo "已关闭定时更新服务"
 else
    echo "错误参数:$regular_update"
-   echo "请输入on/off"
+   echo "请输入 on/off"
+   exit 1
 fi
